373d0da7efb79fd44d030a92ab0db2d93813aae6
BrunoFNRodrigues <bf17669@gmail.com>
2021-09-26 22:40:41 -0300

    Adciona tag ao criar

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self) -> str:
+        return self.name
+
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    tag = models.ForeignKey( Tag, default=None, on_delete=models.CASCADE)
     id = models.AutoField(primary_key=True)
     def __str__(self) -> str:
         return str(self.id)+"."+self.title
+
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Note
+from .models import Note, Tag
 from .form import NoteForm
 
 def index(request):
@@ -8,13 +8,18 @@ def index(request):
         if metodo == "post":
             title = request.POST.get('titulo')
             content = request.POST.get('detalhes')
-            note = Note(title= title, content = content)
-            note.save()
+            tag = request.POST.get('tag')
+            new_tag = Tag(name=tag)         
+            for tag in Tag.objects.all():
+                if new_tag.name == tag.name:
+                    note = Note(title= title, content = content, tag=tag)
+                    note.save()
+                    return redirect('index')
+            new_tag.save()
+            note = Note(title= title, content = content, tag=new_tag)
+            note.save() 
+
         elif metodo == "delete":
-            id = request.POST.get('id')
-            note = Note.objects.get(id=id)
-            note.delete()
-        elif metodo == "upadte":
             id = request.POST.get('id')
             note = Note.objects.get(id=id)
             note.delete()
